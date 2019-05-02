72f0418b68ca69dee5bd2ba2e1c49d4501f665a8
willmann98 <46561913+willmann98@users.noreply.github.com>
2019-05-02 14:46:05 +0200

    Update eksempel18.go

--- eksempel18.go
+++ eksempel18.go
@@ -1,23 +1,25 @@
-package main
+ackage main
 
 import (
-	"io"
-	"log"
-	"os"
+    "os"
+    "log"
+    "io"
 )
 
 func main() {
+   
+    file, err := os.Open("test.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	file, err := os.Open("ww.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	byteSlice := make([]byte, 2)
-	numBytesRead, err := io.ReadFull(file, byteSlice)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Number of bytes read: %d\n", numBytesRead)
-	log.Printf("Data read: %s\n", byteSlice)
+    byteSlice := make([]byte, 512)
+    minBytes := 8
+   
+    numBytesRead, err := io.ReadAtLeast(file, byteSlice, minBytes)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("Number of bytes read: %d\n", numBytesRead)
+    log.Printf("Data read: %s\n", byteSlice)
 }
